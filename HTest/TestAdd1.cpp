40f493a30bfff04088e51d0e20629912da349400
chrishoen <chris123hoen@gmail.com>
2024-09-04 22:55:43 -0400

    more

--- HTest/TestAdd1.cpp
+++ HTest/TestAdd1.cpp
@@ -53,28 +53,33 @@ void TestAdd1::doTest()
       int tInput = 0;
       int tRet = 0;
       char tString[100];
-      printf("%3d + %3d $  ", tValue1, tValue2);
 
-      // Input a string at the cursor.
-      fgets(tString, 99, stdin);
-      if (strlen(tString) == 0)
+      while (true)
       {
-         printf("done\n");
-         return;
-      }
+         printf("%3d + %3d $  ", tValue1, tValue2);
 
-      // Get an integer value from the string.
-      tRet = sscanf(tString, "%d", &tInput);
-      // Test for exit.
-      if (tRet == 0)
-      {
-         printf("done\n");
-         return;
-      }
+         // Input a string at the cursor.
+         fgets(tString, 99, stdin);
+         if (strlen(tString) == 0)
+         {
+            printf("done\n");
+            return;
+         }
 
-      // Test the value.
-      if (tInput != tValue3)
-      {
+         // Get an integer value from the string.
+         tRet = sscanf(tString, "%d", &tInput);
+         // Test for exit.
+         if (tRet == 0)
+         {
+            printf("done\n");
+            return;
+         }
+
+         // Test the value.
+         if (tInput == tValue3)
+         {
+            break;
+         }
          printf("fail\n");
       }
    }
